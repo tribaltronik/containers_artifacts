a7b32f76b323e9fff4c68988f915e474bd98c436
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2022-03-01 09:45:41 +0000

    Add commands

---
+++ commands.sh
@@ -0,0 +1,10 @@
+# Dockerfile_0: Trip Insights - User (Java) API
+# Dockerfile_1: Trip Insights - TripViewer Site
+# Dockerfile_2: Trip Insights - User Profile API
+# Dockerfile_3: Trip Insights - POI (Points Of Interest) API
+# Dockerfile_4: Trip Insights - Trips API
+
+# Authenticate
+az login --use-device-code
+az acr login --name registryhku7094
+
