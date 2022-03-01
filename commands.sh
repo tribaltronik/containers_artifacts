9ffbadb54d156ee16af625e45b96778314766c54
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2022-03-01 10:52:29 +0000

    Upload command

--- commands.sh
+++ commands.sh
@@ -8,6 +8,10 @@
 az login --use-device-code
 az acr login --name registryhku7094
 
+# Build the app container
+cd src/poi
+docker build -t poi -f ..\..\dockerfiles\Dockerfile_3 .
+
 # Create local SQL server container
 docker pull mcr.microsoft.com/mssql/server:2017-latest
 docker run --network bridge -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=MyStrongXYZPassw0rd123" -p 1433:1433 --name sql1 --hostname sql1 -d mcr.microsoft.com/mssql/server:2017-latest
@@ -27,3 +31,6 @@ docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sql1
 
 # Use the IP address to load the data into the DB
 docker run --network bridge -e SQLFQDN=172.17.0.3 -e SQLUSER=sa -e SQLPASS=MyStrongXYZPassw0rd123 -e SQLDB=mydrivingDB registryhku7094.azurecr.io/dataload:1.0
+
+# Run the API container
+docker run --network bridge -e SQL_SERVER=172.17.0.3 -e SQL_USER=sa -e SQL_PASSWORD=MyStrongXYZPassw0rd123 -e SQL_DBNAME=mydrivingDB poi:latest
