fed685411c7e8f9ff5237b9930f470dc4d2b51ba
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2022-03-01 10:12:45 +0000

    Add commands

--- commands.sh
+++ commands.sh
@@ -8,3 +8,10 @@
 az login --use-device-code
 az acr login --name registryhku7094
 
+# Create local SQL server container
+docker pull mcr.microsoft.com/mssql/server:2017-latest
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=MyStrongXYZPassw0rd123" -p 1433:1433 --name sql1 --hostname sql1 -d mcr.microsoft.com/mssql/server:2017-latest
+
+# Login to container
+docker exec -it sql1 /bin/bash
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "MyStrongXYZPassw0rd123"
