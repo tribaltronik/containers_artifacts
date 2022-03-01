58abed088560d8170a9a1fed9db41f4613251644
Jean-Paul van Ravensberg <14926452+DevSecNinja@users.noreply.github.com>
2022-03-01 10:44:56 +0000

    Added database creation

--- commands.sh
+++ commands.sh
@@ -10,8 +10,20 @@ az acr login --name registryhku7094
 
 # Create local SQL server container
 docker pull mcr.microsoft.com/mssql/server:2017-latest
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=MyStrongXYZPassw0rd123" -p 1433:1433 --name sql1 --hostname sql1 -d mcr.microsoft.com/mssql/server:2017-latest
+docker run --network bridge -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=MyStrongXYZPassw0rd123" -p 1433:1433 --name sql1 --hostname sql1 -d mcr.microsoft.com/mssql/server:2017-latest
 
 # Login to container
 docker exec -it sql1 /bin/bash
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "MyStrongXYZPassw0rd123"
+
+# Create database
+## Run in your sqlcmd session
+CREATE DATABASE mydrivingDB;
+GO
+exit
+
+# Get IP address of container
+docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sql1
+
+# Use the IP address to load the data into the DB
+docker run --network bridge -e SQLFQDN=172.17.0.3 -e SQLUSER=sa -e SQLPASS=MyStrongXYZPassw0rd123 -e SQLDB=mydrivingDB registryhku7094.azurecr.io/dataload:1.0
